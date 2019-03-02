4c55bf1eca0cc31364f0d53d147525ed369c1e8d
DEEPAK MALIK <deepakm661@gmail.com>
2019-03-02 17:53:54 +0530

    new commit

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,12 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports = require('./keys_prod')
-  }
-  else {
-      module.exports = require('./keys_dev')
-  }
+module.exports = {
+    mongodb:{
+     dbURI:'mongodb://deepak:deepak123@ds349065.mlab.com:49065/blogging-site'
+    },
+   google:{
+    clientID:'547491143931-em9kk6ugliosahdv3c2ejma7i2cn7vmn.apps.googleusercontent.com',
+    clientSecret:'OPVxeOjzG4Iwdf599p4RYOVS'
+   },
+   session:{
+       cookiekey:'jata#@^%6%^%@^repower#@$%full!!Be@#$23wareofsdafthem' 
+   }
+} 
